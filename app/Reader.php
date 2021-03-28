af116191516207564ab55f8a5521c13693146a9e
Vito Gentile <gentilevito1076@gmail.com>
2021-03-28 12:56:59 +0200

    Creazione variabile fillable nel Model

--- app/Reader.php
+++ app/Reader.php
@@ -6,5 +6,4 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reader extends Model
 {
-    //
-}
+    protected $fillable = ['name', 'lastname', 'birth_date'];//Nella $fillable, dichiaro i campi che posso riempire. 
