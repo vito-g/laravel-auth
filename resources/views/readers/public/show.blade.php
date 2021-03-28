6f18c3acad932c81075cee348af1385d6fa59cdd
Vito Gentile <gentilevito1076@gmail.com>
2021-03-28 21:30:20 +0200

    Show: card inserita

--- resources/views/readers/public/show.blade.php
+++ resources/views/readers/public/show.blade.php
@@ -1,1 +1,22 @@
-Questa è la show
+@extends('readers.template.base')
+
+@section ('title', 'show')
+
+@section('content')
+
+  <div class="card" style= "width: 15rem">
+
+
+    <div class="card-body">
+      <h6 class="card-title">Name: {{$reader->name}}</h6>
+      <h6 class="card-title">Lastname:{{$reader->lastname}}</h6>
+      <h6 class="card-text">Birth-Date: {{$reader->birth_date}}</h6>
+      {{-- <a href="/beers" class="btn btn-primary">All Beers</a> --}}
+
+      {{-- O in alternativa: --}}
+      <a href="{{ route('public-index') }}" class="btn btn-primary">Index</a>
+    </div>
+
+  </div>
+
+@endsection
