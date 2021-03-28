a4a638971019adbb6d2622737278088a36fbf658
Vito Gentile <gentilevito1076@gmail.com>
2021-03-28 17:16:01 +0200

    FX Index & Show in Public Controller

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -7,5 +7,13 @@ use App\Reader;
 
 class PublicController extends Controller
 {
-    //
+  public function index() {
+    $readers = Reader::all();//Preparo una variabile dove vado a mettere tutti i lettori registrati nelle righe del mio database 'readers', creato in MySQL ATTRAVERSO phpMyAdmin, ottenendo un array
+    return view('readers.public.index', compact('readers')); //Gli ritorno una view che ho chiamato index.blade interna alla cartella public del folder delle views "readers" a cui passo l'array salvato nella variabile $readers.
+  }
+
+  public function show(Reader $reader) {
+
+    return view('readers.public.show', compact('reader')); //Gli ritorno una view che ho chiamato show.blade interna alla cartella public del folder delle views "readers" a cui passo l'univo lettore associato al suo id.
+  }
 }
