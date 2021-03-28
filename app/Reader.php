6e58151af428b9c07759b0440683384ffd891332
Vito Gentile <gentilevito1076@gmail.com>
2021-03-28 17:03:25 +0200

    Correzione sul Model Reader

--- app/Reader.php
+++ app/Reader.php
@@ -6,4 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reader extends Model
 {
-    protected $fillable = ['name', 'lastname', 'birth_date'];//Nella $fillable, dichiaro i campi che posso riempire. 
+    protected $fillable = ['name', 'lastname', 'birth_date'];//Nella $fillable, dichiaro i campi che posso riempire.
+}
