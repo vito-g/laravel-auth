51f713a6ffb06de258965caf61064ab619b774f1
Vito Gentile <gentilevito1076@gmail.com>
2021-03-28 18:21:30 +0200

    Template di Cornice: Inserito Header

--- resources/views/readers/template/base.blade.php
+++ resources/views/readers/template/base.blade.php
@@ -10,6 +10,43 @@
 
     <div class="container">
 
+      <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="/readers">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/">Welcome Page</a>
+                </li>
+              </ul>
+              <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
+              @if (!Auth::check())
+                <a class="btn btn-success" href="/login">Login</a>
+              @else
+                <div class="btn btn-danger" aria-labelledby="navbarDropdown">
+                    <a href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </div>
+              @endif
+            </div>
+          </nav>
+    </header>
+
       <main>
         @yield('content')
       </main>
